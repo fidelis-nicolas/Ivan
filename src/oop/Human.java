b2c5cce27cf201c1a443c819a69b5f1a288d199e
Fidelis <fidelisnicolas@gmail.com>
2025-10-29 04:56:22 +0100

    Ecapsulation class

--- src/oop/Human.java
+++ src/oop/Human.java
@@ -2,19 +2,52 @@ package oop;
 
 public class Human {
     //Attributes
-    public String name;
-    public int age;
-    public String sex;
-    public String complexion;
-    public String country;
+    private String name;
+    private int age;
+    private String sex;
+    private String complexion;
+    private String country;
+
+    //final String name = "Fidelis";
+
 
     public Human(String name, int age, String sex, String complexion, String country){
         this.name = name;
         this.age = age;
-        this.sex = sex;
         this.complexion = complexion;
         this.country = country;
+
+
+       if(sex.equalsIgnoreCase("Male")){
+           this.sex = sex;
+       }
+       else if (sex.equalsIgnoreCase("Female")){
+           this.sex = sex;
+       }else{
+           this.sex = null;
+       }
     }
+
+    public Human(int age, String name,  String sex, String country){
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.country = country;
+    }
+    //No Args constructor
+    public Human(){
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    //TODO: Complete
+
+
 
 
     private void eat(){
--- src/oop/HumanApp.java
+++ src/oop/HumanApp.java
@@ -2,9 +2,18 @@ package oop;
 
 public class HumanApp {
     public static void main(String[] args) {
-        Human firstHuman = new Human("Ivan", 67, "male", "White", "Ukraine");// Instance of the Human class
-        //Human secondHuman = new Human();
-        System.out.println(firstHuman.humanInfo());
+        Human firstHuman = new Human("Ivan", 67, "nothing", "White", "Ukraine");// Instance of the Human class
+
+        //TODO: Create 5 humans and add them to the customer array
+        Human [] customers = {firstHuman};
+
+        //TODO: display all human details
+
+        //TODO: Display all human that are male
+
+        //TODO: Display all human that more than 20 years old.
+
+
 
 
     }
